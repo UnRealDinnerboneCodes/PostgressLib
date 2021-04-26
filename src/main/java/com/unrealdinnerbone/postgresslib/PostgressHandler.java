86573e9962ae2511b867fb97d84d5ff40c66984f
UnReal Dinnerbone <unrealdinnerbone@gmail.com>
2021-04-26 16:11:53 -0500

    Code Fix

--- src/main/java/com/unrealdinnerbone/postgresslib/PostgressHandler.java
+++ src/main/java/com/unrealdinnerbone/postgresslib/PostgressHandler.java
@@ -1,7 +1,7 @@
 package com.unrealdinnerbone.postgresslib;
 
-import com.unrealdinnerbone.unreallib.ExceptionConsumer;
 import com.unrealdinnerbone.unreallib.StringUtils;
+import com.unrealdinnerbone.unreallib.exception.ExceptionConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
