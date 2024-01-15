f07407a93aa2c494f57079c15603d4f22c4cdac6
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-01-15 12:30:56 -0600

    Add Array

--- src/main/java/com/unrealdinnerbone/postgresslib/PostgresHandler.java
+++ src/main/java/com/unrealdinnerbone/postgresslib/PostgresHandler.java
@@ -5,26 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class PostgresHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PostgresHandler.class);
-    private final Connection postgres;
+    protected final Connection postgres;
 
     public PostgresHandler(PostgresConfig postgresConfig) throws SQLException {
         String connectionString = StringUtils.replace("jdbc:postgresql://{0}:{1}/{2}?currentSchema={2}", postgresConfig.getHost().get(), postgresConfig.getPort().get(), postgresConfig.getDb().get());
         postgres = DriverManager.getConnection(connectionString, postgresConfig.getUsername().get(), postgresConfig.getPassword().get());
-    }
-
-    public boolean executeUpdate(String script) {
-        try(Statement statement = postgres.createStatement()) {
-            statement.executeUpdate(script);
-            return true;
-        } catch (SQLException e) {
-            LOGGER.error("Error while running script", e);
-            return false;
-        }
     }
 
     public boolean executeUpdate(String quarry, PostgresConsumer preparedStatementConsumer) {
@@ -65,5 +56,9 @@ public class PostgresHandler {
             LOGGER.error("Error while updating", e);
             return null;
         }
+    }
+
+    public Array createArray(String type, Object[] values) throws SQLException {
+        return postgres.createArrayOf(type, values);
     }
 }
